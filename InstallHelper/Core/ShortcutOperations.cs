b9750d3d76e82b7d8d79e5bbe1ac699327bd780f
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2023-08-12 04:09:01 +0200

    Updated Core

---
+++ InstallHelper/Core/ShortcutOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace InstallHelper.Core
+{
+    public class ShortcutOperations
+    {
+        public static void CreateStartMenuShortcut(string path)
+        {
+            string shortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Crypterv2.lnk");
+            CreateShortcut(shortcutPath, path, "Crypterv2 - Encrypt Files");
+        }
+
+        public static void CreateDesktopShortcut(string path)
+        {
+            string shortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Crypterv2.lnk");
+            CreateShortcut(shortcutPath, path, "Crypterv2 - Encrypt Files");
+        }
+
+        public static void CreateShortcut(string shortcutPath, string targetPath, string description)
+        {
+            IWshRuntimeLibrary.WshShell wshShell = new IWshRuntimeLibrary.WshShell();
+            IWshRuntimeLibrary.IWshShortcut shortcut = wshShell.CreateShortcut(shortcutPath) as IWshRuntimeLibrary.IWshShortcut;
+
+            shortcut.TargetPath = targetPath;
+            shortcut.Description = description;
+            shortcut.Save();
+        }
+    }
+}
--- InstallHelper/Program.cs
+++ InstallHelper/Program.cs
@@ -1,30 +1,106 @@
-﻿using Microsoft.Win32;
+﻿using InstallHelper.Core;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using static System.Net.Mime.MediaTypeNames;
-
+using IWshRuntimeLibrary;
+using Microsoft.Win32;
 
 namespace InstallHelper
 {
     public static class Program
     {
+        public static string _uninstallString;
+
         [STAThread]
         public static void Main(string[] args)
         {
+            var processName = "crypterv2";
+
             if (args.Length > 0)
             {
                 for (int i = 0; i < args.Length; i++)
                 {
                     if (args[i].StartsWith("--cp="))
                     {
-                        Console.WriteLine(args[i].Substring(5));
-                        SetCustomFileHandlerAsync(args[i].Substring(5));
-                        RegisterContextMenuEntry(args[i].Substring(5));
+                        var path = args[i].Substring(5);
+
+                        Console.WriteLine("Setup started: v0.2");
+                        
+                        if(Process.GetProcessesByName(processName).Length > 0)
+                        {
+                            foreach(var proc in Process.GetProcessesByName(processName))
+                            {
+                                proc.Kill();
+                            }
+                        }
+
+                        Console.WriteLine("\nKilled all instances of: crypterv2");
+
+                        Console.Write("Updating Registry: ");
+                        SetCustomFileHandlerAsync(path);
+                        RegisterContextMenuEntry(path);
+                        Console.Write("Done!\n");
+
+                        Console.Write("Creating Shortcuts: ");
+
+                        ShortcutOperations.CreateDesktopShortcut(path);
+                        ShortcutOperations.CreateStartMenuShortcut(path);
+
+                        Console.Write("Done!\n");
+
                         Console.ReadKey();
                     }
                 }
 
             }
+            else
+            {
+                Console.WriteLine("Welcome to the Uninstaller!");
+
+                DisplayOptions();
+
+                int choice = GetChoiceFromUser();
+                ProcessUserChoice(choice);
+            }
         }
+
+        public static void DisplayOptions()
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Uninstall Application");
+        }
+
+        public static void ProcessUserChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    UninstallApplication();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+
+        public static void UninstallApplication()
+        {
+            
+        }
+
+        public static int GetChoiceFromUser()
+        {
+            int choice;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out choice))
+                    return choice;
+                Console.WriteLine("Invalid input. Enter a valid choice.");
+            }
+        }
+
+        
 
         public static void RegisterContextMenuEntry(string exePath)
         {
